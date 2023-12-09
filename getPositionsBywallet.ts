9f9c0dcf1144f89d567dbaa13827b9949b05ce6a
bacem <bacem.bergaoui@gmail.com>
2023-12-09 21:56:37 +0100

    add getPositionsByWallet

---
+++ getPositionsBywallet.ts
@@ -0,0 +1,82 @@
+import { BN, AnchorProvider } from "@project-serum/anchor";
+import {
+  WhirlpoolContext, AccountFetcher, buildWhirlpoolClient, ORCA_WHIRLPOOL_PROGRAM_ID,
+  PDAUtil, PriceMath, PoolUtil
+} from "@orca-so/whirlpools-sdk";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { DecimalUtil, TokenUtil } from "@orca-so/common-sdk";
+import { PublicKey } from "@solana/web3.js";
+export async function getPositionsBywalletOrca(publicKey:PublicKey):Promise<any[]>{
+  return new Promise(async (resolve, reject) => {
+    try {
+    const provider = AnchorProvider.env();
+    //@ts-ignore
+    const ctx = WhirlpoolContext.withProvider(provider, ORCA_WHIRLPOOL_PROGRAM_ID);
+    const client = buildWhirlpoolClient(ctx);
+    const fetcher = new AccountFetcher(ctx.connection);
+  
+    const token_accounts = (await ctx.connection.getTokenAccountsByOwner(publicKey, {programId: TOKEN_PROGRAM_ID})).value;
+  
+    const whirlpool_position_candidate_pubkeys = token_accounts.map((ta) => {
+      const parsed = TokenUtil.deserializeTokenAccount(ta.account.data);
+      const pda = PDAUtil.getPosition(ctx.program.programId, parsed.mint);
+      return (parsed?.amount as BN).eq(new BN(1)) ? pda.publicKey : undefined;
+    }).filter(pubkey => pubkey !== undefined);
+  
+    const whirlpool_position_candidate_datas = await fetcher.listPositions(whirlpool_position_candidate_pubkeys, true);
+  
+    const whirlpool_positions = whirlpool_position_candidate_pubkeys.filter((pubkey, i) => 
+      whirlpool_position_candidate_datas[i] !== null
+    );
+  let positions = [];
+      // Output the status of the positions
+      for (let i=0; i < whirlpool_positions.length; i++ ) {
+        const p = whirlpool_positions[i];
+    
+        // Get the status of the position
+        const position = await client.getPosition(p);
+        const data = position.getData();
+    
+        // Get the pool to which the position belongs
+        const pool = await client.getPool(data.whirlpool);
+        const token_a = pool.getTokenAInfo();
+        const token_b = pool.getTokenBInfo();
+        const price = PriceMath.sqrtPriceX64ToPrice(pool.getData().sqrtPrice, token_a.decimals, token_b.decimals);
+    
+        // Get the price range of the position
+        const lower_price = PriceMath.tickIndexToPrice(data.tickLowerIndex, token_a.decimals, token_b.decimals);
+        const upper_price = PriceMath.tickIndexToPrice(data.tickUpperIndex, token_a.decimals, token_b.decimals);
+      
+        // Calculate the amount of tokens that can be withdrawn from the position
+        const amounts = PoolUtil.getTokenAmountsFromLiquidity(
+          data.liquidity,
+          pool.getData().sqrtPrice,
+          PriceMath.tickIndexToSqrtPriceX64(data.tickLowerIndex),
+          PriceMath.tickIndexToSqrtPriceX64(data.tickUpperIndex),
+          true
+        );
+        
+        let positionData = {
+          "position": p.toBase58(),
+          "positionMint": data.positionMint.toBase58(),
+          "whirlpoolAddress": data.whirlpool.toBase58(),
+          "whirlpoolPrice": price.toFixed(token_b.decimals),
+          "tokenA": token_a.mint.toBase58(),
+          "tokenB": token_b.mint.toBase58(),
+          "liquidity": data.liquidity.toString(),
+          "lower": lower_price.toFixed(token_b.decimals),
+          "upper": upper_price.toFixed(token_b.decimals),
+          "amountA": DecimalUtil.fromU64(amounts.tokenA, token_a.decimals).toString(),
+          "amountB": DecimalUtil.fromU64(amounts.tokenB, token_b.decimals).toString(),
+          "decimalA":token_a.decimals,
+          "decimalB":token_b.decimals
+        }
+        positions.push(positionData)
+      }
+    return resolve(positions)
+  }
+  catch (error: any) {
+    return reject(error)
+    }
+})
+}
--- index.ts
+++ index.ts
@@ -1,23 +1,84 @@
 import { BN } from "@project-serum/anchor";
 import { getTokenAmountsFromLiquidity } from "./getTokenAmountsFromLiquidity";
+import { PublicKey } from "@solana/web3.js";
+import { getPositionsBywalletOrca } from "./getPositionsBywallet";
+
+import * as dotenv from 'dotenv';
+import { PriceMath } from "@orca-so/whirlpools-sdk";
+import { DecimalUtil } from "@orca-so/common-sdk";
+dotenv.config();
+
+
 
 // Example parameters - replace these with actual values
-const liquidity = new BN('10');
-const currentSqrtPrice = new BN('10');
-const lowerSqrtPrice = new BN('5');
-const upperSqrtPrice = new BN('20');
-const roundUp = true; // or false
+//const liquidity = new BN('5042021520');
+//const currentSqrtPrice = new BN('73550000');
+//const lowerSqrtPrice = new BN('72400000');
+//const upperSqrtPrice = new BN('75780000');
+//const roundUp = true; // or false
+/*
+function findPriceChangeForOneSolana(liquidity, currentSqrtPrice, lowerSqrtPrice, upperSqrtPrice, increase, tolerance, maxIterations) {
+    let iteration = 0;
+    let sqrtPriceShift = 0;
+    let currentSolana = 0;
+    let initialSolanaPosition = getTokenAmountsFromLiquidity(liquidity, currentSqrtPrice, lowerSqrtPrice, upperSqrtPrice, false).tokenA; // Assuming Solana is tokenA
+
+    while (iteration < maxIterations) {
+        sqrtPriceShift = increase ? sqrtPriceShift + smallIncrement : sqrtPriceShift - smallIncrement; // Adjust the increment as needed
+        let newSolanaPosition = getTokenAmountsFromLiquidity(liquidity, currentSqrtPrice + sqrtPriceShift, lowerSqrtPrice, upperSqrtPrice, false).tokenA;
+
+        currentSolana = newSolanaPosition - initialSolanaPosition;
+        if (Math.abs(currentSolana - 1) < tolerance) {
+            break;
+        }
+
+        iteration++;
+    }
+
+    return currentSqrtPrice + sqrtPriceShift;
+}
+*/
+//let priceChange = findPriceChangeForOneSolana(liquidity, currentSqrtPrice, lowerSqrtPrice, upperSqrtPrice, true /* increase */, 0.01 /* tolerance */, 100 /* max iterations */);
+
+//console.log(priceChange);
 
 async function main() {
     try {
-        const result = await getTokenAmountsFromLiquidity(
+
+
+        const wallet = new PublicKey("");
+        const liquidityData = await getPositionsBywalletOrca(wallet);
+        console.log(liquidityData)
+
+
+      /* const result = await getTokenAmountsFromLiquidity(
             liquidity, 
             currentSqrtPrice, 
             lowerSqrtPrice, 
             upperSqrtPrice, 
             roundUp
-        );
-        console.log("Result:", result);
+        );*/
+        
+        let liquidity = liquidityData[0];
+
+        //@ts-ignore
+        let lowerPriceBound = liquidity.lower;
+        console.log(" lower = " + lowerPriceBound)
+        //@ts-ignore
+        let upperPriceBound = liquidity.upper;
+        //@ts-ignore
+        let futurePrice =  liquidity.whirlpoolPrice * 1.01;
+        console.log("future = " ,  futurePrice);
+        //@ts-ignore
+        let pc = PriceMath.priceToSqrtPriceX64(DecimalUtil.fromNumber(futurePrice), liquidity.decimalA, liquidity.decimalB)
+            //console.log(pc)
+            //@ts-ignore
+            let pl = PriceMath.priceToSqrtPriceX64(DecimalUtil.fromNumber(lowerPriceBound), liquidity.decimalA, liquidity.decimalB)
+            //@ts-ignore
+            let pu = PriceMath.priceToSqrtPriceX64(DecimalUtil.fromNumber(upperPriceBound), liquidity.decimalA, liquidity.decimalB)
+           //@ts-ignore
+            await getTokenAmountsFromLiquidity(liquidity.liquidity, pc, pl, pu, false);
+
     } catch (error) {
         console.error("Error:", error);
     }
